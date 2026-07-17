e7d17547133c4c7d1f4fe86f7a928e60ab9d997b
Ixee <buffjokester27@gmail.com>
2026-07-17 19:24:24 +0530

    Android: More Layout Improvements

--- engine/Poseidon/Input/InputProcessingSdl.cpp
+++ engine/Poseidon/Input/InputProcessingSdl.cpp
@@ -57,12 +57,12 @@ struct VirtualButtonDef {
 VirtualButtonDef g_mobileButtons[] = {
     {"Fire", 0.08f, 0.33f, 0.11f, VB_FIRE_LOOK, 0, 0}, // LEFT FIRE
     {"Move", 0.15f, 0.7f, 0.12f, VB_WASD, 0},
-    {"ADS", 0.94f, 0.45f, 0.07f, VB_KEY, SDL_SCANCODE_V},
-    {"Zoom", 0.78f, 0.49f, 0.07f, VB_ZOOM_LOOK, 0},
+    {"ADS", 0.95f, 0.48f, 0.07f, VB_KEY, SDL_SCANCODE_V},
+    {"Zoom", 0.84f, 0.5f, 0.07f, VB_ZOOM_LOOK, 0},
     {"Fire", 0.85f, 0.65f, 0.09f, VB_FIRE_LOOK, 0},
-    {"Action\n(Drag)", 0.875f, 0.47f, 0.07f, VB_ACTION, 0},
+    {"Action\n(Drag)", 0.875f, 0.49f, 0.07f, VB_ACTION, 0},
     {"Freelook", 0.88f, 0.34f, 0.05f, VB_LOOK, SDL_SCANCODE_LALT, 0},
-    {"Reload", 0.95f, 0.34f, 0.06f, VB_KEY, SDL_SCANCODE_R},
+    {"Reload", 0.95f, 0.30f, 0.05f, VB_KEY, SDL_SCANCODE_R},
     {"Q", 0.95f, 0.74f, 0.05f, VB_KEY, SDL_SCANCODE_Q},
     {"Crouch", 0.95f, 0.85f, 0.06f, VB_KEY, SDL_SCANCODE_Q}, // INTEGRATE IT WITH PRONE (Z), TAP TO CROUCH, HOLD TO PRONE
     {"Prone", 0.85f, 0.85f, 0.06f, VB_KEY, SDL_SCANCODE_Z},
@@ -71,7 +71,7 @@ VirtualButtonDef g_mobileButtons[] = {
     {"Map", 0.88f, 0.08f, 0.05f, VB_KEY, SDL_SCANCODE_M},
     {"ESC", 0.96f, 0.08f, 0.05f, VB_KEY, SDL_SCANCODE_ESCAPE},
     {"Bino", 0.79f, 0.20f, 0.06f, VB_KEY, SDL_SCANCODE_B},
-    {"Firemode", 0.96f, 0.20f, 0.06f, VB_KEY, SDL_SCANCODE_SPACE},
+    {"Firemode", 0.96f, 0.20f, 0.05f, VB_KEY, SDL_SCANCODE_SPACE},
     {"TP/FP", 0.70f, 0.20f, 0.05f, VB_KEY, SDL_SCANCODE_KP_ENTER},
     {"Shift", 0.95f, 0.62f, 0.05f, VB_KEY, SDL_SCANCODE_LSHIFT},
     {"Chat/Channels", 0.88f, 0.20f, 0.05f, VB_KEY, SDL_SCANCODE_SLASH},
